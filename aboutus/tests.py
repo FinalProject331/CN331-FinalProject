d2166b42a39d017aac9f636166f0b0e09c3b068b
Supakrit Nithikethkul <supakrit.nit@dome.tu.ac.th>
2022-10-25 23:03:07 +0700

    test

--- aboutus/tests.py
+++ aboutus/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
 # Create your tests here.
+
+# views_test
+
+class AboutusTestCase(TestCase):
+    def setup(self):
+        self.client = Client()
+
+    def test_views_aboutus(self):
+
+        c = Client()
+        response = c.get(reverse('aboutus'))
+        self.assertEqual(response.status_code, 200)
