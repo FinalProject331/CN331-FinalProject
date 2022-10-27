d4b14018536b4bd3f7ac46b4e4ebb281cdd10f03
Supakrit Nithikethkul <supakrit.nit@dome.tu.ac.th>
2022-10-28 02:32:59 +0700

    renovate

--- FWB/urls.py
+++ FWB/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('home/', include('aboutus.urls')),
+    path('', include('aboutus.urls')),
 ]
