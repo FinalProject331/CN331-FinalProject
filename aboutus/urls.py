ada772a3cad9acba9fe7e8e7a38fae64504d58a1
Supakrit Nithikethkul <supakrit.nit@dome.tu.ac.th>
2022-10-27 18:08:18 +0700

    last pond day

--- aboutus/urls.py
+++ aboutus/urls.py
@@ -1,9 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
+
 
 urlpatterns = [
     path('', views.login, name='login'),
     path('aboutus', views.aboutus, name='aboutus'),
     path('signup', views.signup, name='signup'),
+    path('signup/create_user', views.create_user, name='create_user'),
+    path('home', views.home, name='home'),
 ]
--- aboutus/views.py
+++ aboutus/views.py
@@ -1,9 +1,12 @@
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, logout
+from django.contrib.auth import login as auth_login
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.models import User
+
+from aboutus.models import Account
 
 # Create your views here.
 
@@ -16,8 +19,8 @@ def login(request):
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse('users:index'))
+            auth_login(request, user)
+            return HttpResponseRedirect(reverse('home'))
         else:
             messages.warning(request, "Invalid credential.")
             return render(request, "accounts/login.html", {
@@ -36,7 +39,18 @@ def logout_view(request):
 def signup(request):
     return render(request, "accounts/signup.html")
 
-def create_user(request, email,name, password, birthday):
-    user = User.objects.create_user(name, email, password)
-    
+def create_user(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
+        birthday = request.POST["birthday"]
+        uname = request.POST["username"]
+    user = User.objects.create_user(uname, email, password)
+    account = Account.objects.create(user=user, birthday=birthday)
     return render(request, "accounts/login.html")
+
+def home(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    else:
+        return render(request, "home/home.html")
