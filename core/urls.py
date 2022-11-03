caedc4c8411f6b371feec5c3e208781b86f8ea0b
Natiphon Chanphet <6410615030@cn103>
2022-11-03 18:24:41 +0700

    last pull

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
+from chat.views import roomconfig
 
 
 urlpatterns = [
-    # path('', views.login, name='login'),
     path('aboutus', views.aboutus, name='aboutus'),
     path('help', views.help, name='help'),
     
     path('', views.home, name = "home"),
     path("signup/", views.SignUp.as_view(), name="signup"),
+    path('roomconfig', roomconfig, name='roomconfig'),
 ]
