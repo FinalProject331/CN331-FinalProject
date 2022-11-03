1b4d3acde5a71e15364a4a5ebb09083e3cf42380
6410685033 <6410685033@student.tu.ac.th>
2022-11-03 15:37:05 +0700

    pull

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 from django.urls import path, include
-=======
-import imp
-from django.urls import path
-from chat.views import room
->>>>>>> cc0a395 (test room)
 from . import views
 from chat.views import roomconfig
 urlpatterns = [
@@ -15,10 +9,6 @@ urlpatterns = [
     # path('home', views.home, name='home'),
     path('', views.home, name = "home"),
     path("signup/", views.SignUp.as_view(), name="signup"),
-<<<<<<< HEAD
     path('roomconfig', roomconfig, name='roomconfig'),
 
-=======
-    path('<str:room>/', room, name='room'),
->>>>>>> cc0a395 (test room)
 ]
