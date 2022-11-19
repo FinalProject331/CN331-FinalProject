430e61fd1e3e2c04df38624a14a993e6f4eebe2a
Supapan Ngorsakun <6410615147@cn103>
2022-11-19 19:10:10 +0700

    default img

--- account/models.py
+++ account/models.py
@@ -10,7 +10,7 @@ class Account(models.Model):
 	birthday = models.DateField(default=datetime.now)
 	user = models.OneToOneField(User,on_delete=models.CASCADE ,related_name='profile') 
 	bio = models.CharField(max_length=300, default="")
-	image = models.ImageField(default='default.jpg', upload_to='profile_pics/',blank=True, null=True)
+	image = models.ImageField(default='default.png', upload_to='profile_pics/',blank=True, null=True)
 	chat = models.IntegerField(default=0)
 	GenderType = [('M','Male'),
 					('F', 'Female'),
